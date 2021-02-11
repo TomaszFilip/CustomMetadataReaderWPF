095d04babc6b91db6b7a3fcf3e8c30cf99690617
Tomasz Filip <tomasz.filip@etongroup.eu>
2021-02-11 11:08:02 +0100

    int

---
+++ MetadataReader.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32;
+using Syncfusion.Pdf.Parsing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CustomMetadataReaderWPF
+{
+    public class MetadataReader
+    {
+        private PdfLoadedDocument Document;
+
+        public void Open()
+        {
+            OpenFileDialog openFileDlg = new OpenFileDialog
+            {
+                DefaultExt = "pdf",
+                Filter = "Pdf (*.pdf)|*.pdf",
+            };
+            openFileDlg.ShowDialog();
+            var path = openFileDlg.FileName;
+            Stream stream = File.Open(path, FileMode.Open);
+            Document = new PdfLoadedDocument(stream);
+            string custommetadata = Document.DocumentInformation.CustomMetadata["TestData"];
+        }
+    }
+}
